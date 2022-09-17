4d8ef729e7b74aa9f91f8271406e905af9227d3d
terry_u16 <terry-u16@users.noreply.github.com>
2022-09-17 16:33:26 +0900

    Vec2を実装

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -3,6 +3,8 @@ use proconio::*;
 #[allow(unused_imports)]
 use rand::prelude::*;
 
+use crate::vector::Vec2;
+
 #[allow(unused_macros)]
 macro_rules! chmin {
     ($base:expr, $($cmps:expr),+ $(,)*) => {{
@@ -65,5 +67,85 @@ macro_rules! mat {
 struct Input {}
 
 fn main() {
+    let v = Vec2::new(0, 0);
+    eprintln!("{}", -v);
     todo!();
+}
+
+mod vector {
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+    pub struct Vec2 {
+        x: i32,
+        y: i32,
+    }
+
+    impl Vec2 {
+        pub const fn new(x: i32, y: i32) -> Self {
+            Self { x, y }
+        }
+
+        pub const fn in_map(&self, n: usize) -> bool {
+            let n = n as u32;
+            (self.x as u32) < n && (self.y as u32) < n
+        }
+
+        pub const fn rot90(&self) -> Self {
+            Self::new(-self.y, self.x)
+        }
+    }
+
+    impl std::ops::Add for Vec2 {
+        type Output = Vec2;
+
+        fn add(self, rhs: Self) -> Self::Output {
+            Vec2::new(self.x + rhs.x, self.y + rhs.y)
+        }
+    }
+
+    impl std::ops::AddAssign for Vec2 {
+        fn add_assign(&mut self, rhs: Self) {
+            self.x += rhs.x;
+            self.y += rhs.y;
+        }
+    }
+
+    impl std::ops::Sub for Vec2 {
+        type Output = Vec2;
+
+        fn sub(self, rhs: Self) -> Self::Output {
+            Vec2::new(self.x - rhs.x, self.y - rhs.y)
+        }
+    }
+
+    impl std::ops::SubAssign for Vec2 {
+        fn sub_assign(&mut self, rhs: Self) {
+            self.x -= rhs.x;
+            self.y -= rhs.y;
+        }
+    }
+
+    impl std::fmt::Display for Vec2 {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            write!(f, "({} {})", self.x, self.y)
+        }
+    }
+
+    impl std::ops::Neg for Vec2 {
+        type Output = Vec2;
+
+        fn neg(self) -> Self::Output {
+            Vec2::new(-self.x, -self.y)
+        }
+    }
+
+    pub const UNITS: [Vec2; 8] = [
+        Vec2::new(1, 0),
+        Vec2::new(1, 1),
+        Vec2::new(0, 1),
+        Vec2::new(-1, 1),
+        Vec2::new(-1, 0),
+        Vec2::new(-1, -1),
+        Vec2::new(0, -1),
+        Vec2::new(1, -1),
+    ];
 }
