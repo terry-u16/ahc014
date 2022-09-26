6406f9a314d38b259f42cfb1338e31a8961cf222
terry_u16 <terry-u16@users.noreply.github.com>
2022-09-26 23:40:08 +0900

    コンフィグ修正

--- Generate-Movie.ps1
+++ Generate-Movie.ps1
@@ -7,6 +7,7 @@ param(
 $in = ".\data\in\{0:0000}.txt" -f $seed
 $out = "out.txt"
 $env:MOVIE = "true"
+$env:DURATION_MUL = "0.6"
 Get-Content $in | cargo run --bin ahc014-a --release > $out
 Remove-Item env:MOVIE
 dotnet run --project .\Visualizer\Visualizer\ -- -i $in -s $out -o out.mp4
--- Start-SingleCase.ps1
+++ Start-SingleCase.ps1
@@ -5,5 +5,6 @@ param(
 )
 
 $in = ".\data\in\{0:0000}.txt" -f $seed
+$env:DURATION_MUL = "0.6"
 Get-Content $in | cargo run --bin ahc014-a --release > out.txt
 .\vis.exe $in out.txt
