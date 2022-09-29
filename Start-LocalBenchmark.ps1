ade0a6961fad3f06c606148280dd6808f908225d
terry_u16 <terry-u16@users.noreply.github.com>
2022-09-30 00:26:30 +0900

    ツール修正

--- Start-LocalBenchmark.ps1
+++ Start-LocalBenchmark.ps1
@@ -1,6 +1,6 @@
 Write-Host "[Compile]"
 cargo build --release --bin ahc014-a
-Move-Item ../target/release/ahc014-a.exe .
+Move-Item ../target/release/ahc014-a.exe . -Force
 Write-Host "[Run]"
 $env:DURATION_MUL = "0.6"
 dotnet marathon run-local
